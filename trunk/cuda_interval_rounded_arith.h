b177b108a538263e5f73256106e378e8ec86ec7e
chacarero@gmail.com <chacarero@gmail.com@574b3456-9212-fb5f-372f-6fb53f4479ee>
2013-08-16 14:43:44 +0000

    Deleting some old comments...

--- trunk/cuda_interval_rounded_arith.h
+++ trunk/cuda_interval_rounded_arith.h
@@ -1,14 +1,3 @@
-/*
- * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
-
 // Type-specific implementation of rounded arithmetic operators.
 // Thin layer over the CUDA intrinsics.
 
